50c9bc62e975fe39c48a6a6d7cbac77dbf8e2227
lamijazukan <zukanlamija@gmail.com>
2026-02-02 19:56:28 +0100

    Update database migrations for budget and expense fields

---
+++ ExpenseTrackerApp/src/ExpenseTrackerApp.Infrastructure/Migrations/20260128222714_updateBudgetEntity.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTrackerApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateBudgetEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Category_UserId",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "PeriodType",
+                table: "Budget");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_UserId_ParentCategoryId_Name",
+                table: "Category",
+                columns: new[] { "UserId", "ParentCategoryId", "Name" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Category_UserId_ParentCategoryId_Name",
+                table: "Category");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PeriodType",
+                table: "Budget",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_UserId",
+                table: "Category",
+                column: "UserId");
+        }
+    }
+}

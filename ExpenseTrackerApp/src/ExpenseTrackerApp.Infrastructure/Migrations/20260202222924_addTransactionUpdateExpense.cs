34b1c751344760cab1fff2bc89860c594b9a12f3
lamijazukan <zukanlamija@gmail.com>
2026-02-02 23:33:32 +0100

    Add Transaction and Expense entities

---
+++ ExpenseTrackerApp/src/ExpenseTrackerApp.Infrastructure/Migrations/20260202222924_addTransactionUpdateExpense.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ExpenseTrackerApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTransactionUpdateExpense : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expense_Users_UserId",
+                table: "Expense");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Expense_UserId",
+                table: "Expense");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Expense");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Expense");
+
+            migrationBuilder.DropColumn(
+                name: "ExpenseDate",
+                table: "Expense");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "Expense");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Expense");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "Expense",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionId",
+                table: "Expense",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PaidDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Store = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "numeric(12,2)", precision: 12, scale: 2, nullable: false),
+                    PaymentMethod = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionId);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expense_TransactionId",
+                table: "Expense",
+                column: "TransactionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expense_Transactions_TransactionId",
+                table: "Expense",
+                column: "TransactionId",
+                principalTable: "Transactions",
+                principalColumn: "TransactionId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expense_Transactions_TransactionId",
+                table: "Expense");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Expense_TransactionId",
+                table: "Expense");
+
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "Expense");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionId",
+                table: "Expense");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Expense",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Expense",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "ExpenseDate",
+                table: "Expense",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMethod",
+                table: "Expense",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Expense",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expense_UserId",
+                table: "Expense",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expense_Users_UserId",
+                table: "Expense",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

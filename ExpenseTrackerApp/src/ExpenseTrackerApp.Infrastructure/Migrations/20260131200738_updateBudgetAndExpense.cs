ace911c01bf60ca42cb7bcce1bfa701e9a592c33
lamijazukan <zukanlamija@gmail.com>
2026-02-02 19:56:28 +0100

    Update database migrations for budget and expense fields

---
+++ ExpenseTrackerApp/src/ExpenseTrackerApp.Infrastructure/Migrations/20260131200738_updateBudgetAndExpense.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTrackerApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateBudgetAndExpense : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReviewed",
+                table: "Expense");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Budget");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReviewed",
+                table: "Expense",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Budget",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+        }
+    }
+}

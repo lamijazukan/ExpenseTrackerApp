4ec430211597c2c14d1d6baba2f72389bb8d4ad9
lamijazukan <zukanlamija@gmail.com>
2026-02-03 03:18:17 +0100

    Remove one isReviewed from BudgetResult and add migrations

---
+++ ExpenseTrackerApp/src/ExpenseTrackerApp.Infrastructure/Migrations/20260203010122_fixTransactionEntityDbNameMismatch.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTrackerApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixTransactionEntityDbNameMismatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expense_Transactions_TransactionId",
+                table: "Expense");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Users_UserId",
+                table: "Transactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Transactions",
+                table: "Transactions");
+
+            migrationBuilder.RenameTable(
+                name: "Transactions",
+                newName: "Transaction");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transaction",
+                newName: "IX_Transaction_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Transaction",
+                table: "Transaction",
+                column: "TransactionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expense_Transaction_TransactionId",
+                table: "Expense",
+                column: "TransactionId",
+                principalTable: "Transaction",
+                principalColumn: "TransactionId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transaction_Users_UserId",
+                table: "Transaction",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expense_Transaction_TransactionId",
+                table: "Expense");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transaction_Users_UserId",
+                table: "Transaction");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Transaction",
+                table: "Transaction");
+
+            migrationBuilder.RenameTable(
+                name: "Transaction",
+                newName: "Transactions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transaction_UserId",
+                table: "Transactions",
+                newName: "IX_Transactions_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Transactions",
+                table: "Transactions",
+                column: "TransactionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expense_Transactions_TransactionId",
+                table: "Expense",
+                column: "TransactionId",
+                principalTable: "Transactions",
+                principalColumn: "TransactionId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Users_UserId",
+                table: "Transactions",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
